df88d93b1821852bad908691dae84c040f65f836
bar_pariente <bar.pariente@post.runi.ac.il>
2024-01-31 20:35:16 +0200

    last

--- SpellChecker.java
+++ SpellChecker.java
@@ -17,21 +17,27 @@ public class SpellChecker {
 
 	public static int levenshtein(String word1, String word2) {
 		// Your code goes here
-		if ((word1.length()==0) || (word2.length() == 0)){
-			return (Math.max(word1.length(), word2.length())); 
+		word1 = word1.toLowerCase(); 
+		word2 = word2.toLowerCase(); 
+		if (word1.length()==0){
+			return word2.length(); 
+		}
+		else if (word2.length()==0){
+			return word1.length(); 
 		}
 
-		if (word1.charAt(0) == (word2.charAt(0))){
-			levenshtein(tail(word1),tail(word2)); 
+		else if (word1.charAt(0) == (word2.charAt(0))){
+			return levenshtein(tail(word1),tail(word2)); 
 		}
-		if (levenshtein(tail(word1),word2) > levenshtein(word1,tail(word2))){
-			
-			return 1 + Math.min(levenshtein(word1,tail(word2)) , levenshtein(tail(word1),tail(word2)));
-		}
-		else {
-			return 1 + Math.min(levenshtein(tail(word1),word2) , levenshtein(tail(word1),tail(word2)));
-		}
+
+		int option1 = levenshtein(tail(word1), tail(word2));
+  		int option2 = levenshtein(word1, tail(word2));
+		int option3 = levenshtein(tail(word1), word2);
+
+    	return 1 + Math.min(option1, Math.min(option2, option3));
+	
 	}
+	
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
